d369d6d8a8b3a38df3c8077b2c7d45df87675e0e
sashavining <96848086+sashavining@users.noreply.github.com>
2022-09-26 15:44:00 -0400

    Basic question fade in handled

---
+++ src/components/FadingQuestionText.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const FadingQuestionText = ({question, toggleAnswering, setDisplayString, displayString, questionInterval}) => {
+    useEffect(() => {
+        setDisplayString('');
+        let index = 0;
+
+        let addChar = setInterval(tick, questionInterval);
+
+        function tick() {
+            setDisplayString(() => question.slice(0, index+1));
+            index++;
+            if (index === question.length - 1) {
+                clearInterval(addChar)
+                toggleAnswering()
+            } 
+        }
+        return () => clearInterval(addChar);
+
+    }, [question])
+
+    return(displayString)
+} 
+
+export default FadingQuestionText
---
+++ src/components/TimedSelectionScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import CategorySelect from './forms/CategorySelect';
+import GameTypeSelect from './forms/GameTypeSelect';
+import QuestionIntervalSelect from './forms/QuestionIntervalSelect';
+
+const TimedSelectionScreen = ({setCategory, setMode, setQuestionInterval, startPlaying}) => {
+    const [currentCategory, setCurrentCategory] = useState('');
+    const [currentMode, setCurrentMode] = useState('')
+    const [currentInterval, setCurrentInterval] = useState(0)
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (currentMode === '') {
+            return (
+            <form>
+                <CategorySelect setCurrentCategory={setCurrentCategory} />
+                <GameTypeSelect setGameType={setCurrentMode} />
+                <QuestionIntervalSelect setCurrentInterval={setCurrentInterval} />
+
+                <button type="submit" onClick={(e) => handleSubmit(e)}>Submit</button>
+            </form> )
+        }
+        setCategory(currentCategory)
+        setMode(currentMode)
+        setQuestionInterval(currentInterval)
+        startPlaying()    
+        
+    }    
+    
+    return (
+       <form onSubmit={(e) => handleSubmit(e)}>
+        <h2 class="text-center mb-3">Game Select:</h2>
+        <CategorySelect setCurrentCategory={setCurrentCategory} />
+        <GameTypeSelect setGameType={setCurrentMode} />
+        <QuestionIntervalSelect setCurrentInterval={setCurrentInterval} />
+
+        <button type="submit" onClick={(e) => handleSubmit(e)}>Submit</button>
+       </form>    
+       )
+}
+  
+
+export default TimedSelectionScreen
