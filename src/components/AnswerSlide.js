6540f1d163e0761fd571d8f50456a8aad45b0c4a
sashavining <96848086+sashavining@users.noreply.github.com>
2022-09-27 11:57:06 -0400

    Optimal buzz in index/time feature added

--- src/components/AnswerSlide.js
+++ src/components/AnswerSlide.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
 
-const AnswerSlide = ({wasCorrect, handleQuestionFlow, correctAnswer, buzzedInString, idealBuzzedInString, questionText, fadeInterval}) => {
+const AnswerSlide = ({wasCorrect, handleQuestionFlow, correctAnswer, buzzedInString, optimalWord, questionText, questionInterval}) => {
+
+    const findBuzzInIndex = (word, questionText) => {
+        let startingIndex = questionText.indexOf(word)
+        return (startingIndex + word.length)
+    }
+
+    const calculateDifferenceBetweenIdealAndActualInSeconds = (optimalWord, questionText, actualBuzzedInString, intervalInMs) => {
+        let idealBuzzedInIndex = findBuzzInIndex(optimalWord, questionText)
+        console.log(idealBuzzedInIndex * intervalInMs)
+        return (((actualBuzzedInString.length - 1) - idealBuzzedInIndex) * intervalInMs) / 1000
+    }
+
+    const differenceBetweenIdealAndActualBuzz = calculateDifferenceBetweenIdealAndActualInSeconds(optimalWord, questionText, buzzedInString, questionInterval)
+    
+    const lastWordOfActualBuzz = buzzedInString.split(" ").pop()
 
     return(
         <>
         <div class="feedback-text">
             {((wasCorrect) && "Correct! Great job.")}
             {((!wasCorrect) && `Incorrect! The answer we were looking for was: ${correctAnswer}`)}
-            {(buzzedInString && idealBuzzedInString) && (
+            {(buzzedInString && optimalWord) && (
                 <>
                     <p>{`The question was: ${questionText}`}</p>
-                    <p>{`You buzzed in at ${buzzedInString}. The ideal buzz was at ${idealBuzzedInString}`}</p>
-                    {/* get the index of the  */}
+                    <p>{`The ideal buzz was at ${optimalWord.trim()}`}</p>
+                    <p>{(differenceBetweenIdealAndActualBuzz === 0) && `You buzzed in at the optimal time! Congratulations`}</p>
+                    <p>{(differenceBetweenIdealAndActualBuzz > 0) && `You buzzed in ${differenceBetweenIdealAndActualBuzz} seconds later than optimal!`}</p>
+                    <p>{(differenceBetweenIdealAndActualBuzz < 0) && `You buzzed in ${Math.abs(differenceBetweenIdealAndActualBuzz)} seconds earlier than optimal!`}</p>
                 </>
             )}
         </div>
-{/* question text - passed in as a a prop */}
-{/* speed interval to calculate what the difference between buzzed + ideal is */}
-
         <button onClick={(e) => handleQuestionFlow(e, wasCorrect)}>Next</button>
         </>
     )
